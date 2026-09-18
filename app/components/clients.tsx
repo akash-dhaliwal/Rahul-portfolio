f7b422f77cd24e26046208c4b0ed3248b8e12cd0
Akash Dhaliwal <silentlikhari@gmail.com>
2026-09-18 22:05:41 +0530

    Update clients.tsx

--- app/components/clients.tsx
+++ app/components/clients.tsx
@@ -66,7 +66,7 @@ export default function Clients() {
             {clients.map((logo, index) => (
               <div
                 key={`client-${index}`}
-                className="flex w-1/5 min-w-0 items-center justify-center overflow-visible px-[5px]"
+                className="flex w-1/5 min-w-0 items-center justify-center overflow-visible px-[2px]"
               >
                 <Image
                   src={logo}
